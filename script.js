d97e996debe2ec7c646034f5aae201da8ee99fdd
rsaito1216 <70747012+rsaito1216@users.noreply.github.com>
2021-06-17 20:46:07 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+//sticky header
+  $(function() {
+    var $win = $(window),
+        $fv = $('.fv'),
+        $header = $('.header')
+        fvHeight = $fv.outerHeight();
+  
+    $win.on('load scroll',function(){
+      var value = $(this).scrollTop();
+        if($win.width() > 768){
+          if ( value > fvHeight){
+            $header.addClass("fixed");
+          } else {
+            $header.removeClass("fixed");
+          }
+        }
+    });
+  });
+
+  //ハンバーガーメニューの開閉
+  $('.burger-btn').on('click',function(){
+    $('.header-nav').fadeToggle(300);
+    $('.burger-btn').toggleClass('cross');
+    $('body').toggleClass('noscroll');
+  });
+
+
+  //ファーストビューのslider
+  $('.fv-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+
+  
+
